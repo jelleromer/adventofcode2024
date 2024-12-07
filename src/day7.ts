807c392d8f625e84032c7ad567ca1e9c3cf0a923
jromer <jelleromer@live.nl>
2024-12-07 15:09:08 +0100

    day 7

--- src/day7.ts
+++ src/day7.ts
@@ -2,7 +2,7 @@ import { curry, sum, zip } from '@es-toolkit/es-toolkit'
 import { findAll, print } from './utils.ts'
 
 function countDigits(x: number) {
-    return Math.floor(Math.log10(x) + 1);
+  return Math.floor(Math.log10(x) + 1)
 }
 
 type numFunc = (a: number, b: number) => number
@@ -10,41 +10,46 @@ const plus: numFunc = (a, b) => a + b
 const mult: numFunc = (a, b) => a * b
 const concatnum: numFunc = (a, b) => 10 ** countDigits(b) * a + b
 
+type genFunc = (l: number) => numFunc[][]
+
 function generateCombinations1(length: number): numFunc[][] {
-    if (length === 0) return [[]];
-    const smallerCombinations = generateCombinations1(length - 1);
-    return [
-        ...smallerCombinations.map(xs => [...xs, plus]),
-        ...smallerCombinations.map(xs => [...xs, mult]),
-    ];
+  if (length === 0) return [[]]
+  const smallerCombinations = generateCombinations1(length - 1)
+  return [
+    ...smallerCombinations.map((xs) => [...xs, plus]),
+    ...smallerCombinations.map((xs) => [...xs, mult]),
+  ]
 }
 
 function generateCombinations2(length: number): numFunc[][] {
-    if (length === 0) return [[]];
-    const smallerCombinations = generateCombinations2(length - 1);
-    return [
-        ...smallerCombinations.map(xs => [...xs, plus]),
-        ...smallerCombinations.map(xs => [...xs, mult]),
-        ...smallerCombinations.map(xs => [...xs, concatnum]),
-    ];
+  if (length === 0) return [[]]
+  const smallerCombinations = generateCombinations2(length - 1)
+  return [
+    ...smallerCombinations.map((xs) => [...xs, plus]),
+    ...smallerCombinations.map((xs) => [...xs, mult]),
+    ...smallerCombinations.map((xs) => [...xs, concatnum]),
+  ]
 }
 
-function oneLine(genFunc: (l: number) => numFunc[][], line: [number, number[]]) {
+function oneLine(f: genFunc, line: [number, number[]]) {
   const [total, parts] = line
-  const res = genFunc(parts.length - 1)
-    .map((op) => zip(op, parts.slice(1)))
-    .map((operators) => {
-      return operators.reduce((acc: number, a: [numFunc, number]) => {
-        const [op, n] = a
-        return op(acc, n)
-      }, parts[0])
+  const res = f(parts.length - 1)
+    .map((ops) => zip(ops, parts.slice(1)))
+    .map((pairs) => {
+      return pairs.reduce(
+        (a: number, b: [numFunc, number]) => {
+          const [op, n] = b
+          return op(a, n)
+        },
+        parts[0],
+      )
     })
     .filter((x) => x === total)
   return res[0] ?? 0
 }
 
-function solve(xs: [number, number[]][], genFunc: (l: number) => numFunc[][]) {
-  return sum(xs.map(curry(oneLine)(genFunc)))
+function solve(xs: [number, number[]][], f: genFunc) {
+  return sum(xs.map(curry(oneLine)(f)))
 }
 
 export function runDay() {
